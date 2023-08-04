78d5af923ef0b827f90fdce075b28f70c906c94e
Alpha-1729 <10110011100011110000@tutanota.com>
2023-08-05 03:18:45 +0530

    Added new topics

---
+++ 011-GIT-Terminal_Crash_Course/Program.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Terminal Crash Course
+
+:"
+>>>>
+>>>>
+>>>>
+>>>>
+"
+
+# Opening file explorer in the current folder in Mac.
+open .
+
+# Opening file explorer in the current folder in Windows.
+start .
+
+# Create multiple files with touch.
+touch hello.txt app.py index.html
+
+# Remove a non-empty folder.
+rm -rf FolderName
+
+# Shortcut to clear the terminal in Mac.
+Cmd + K
