d2083dca91d0cb453b6781d3e39e4a8724bef164
Alpha-1729 <10110011100011110000@tutanota.com>
2023-08-12 17:33:01 +0530

    Completed Section 7: Merging Branches

--- 029-GIT-Branch_Command/Program.sh
+++ 029-GIT-Branch_Command/Program.sh
@@ -4,7 +4,8 @@
 :"
 >>>> Git branch Docs
         Reference Link: https://git-scm.com/docs/git-branch
->>>>
+>>>> Show all branches and last commit in that branch.
+        git branch -v
 >>>>
 >>>>
 "
---
+++ 038-GIT-Resolving_Merge_Conflicts/Program.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Resolving Merge Conflicts
+
+:"
+>>>> If you want to abort the merge due to conflict.
+        git merge --abort
+>>>>
+>>>>
+>>>>
+"
