f3517ce77b0cfd975deaa64ebe38ef5761d2cdaf
Alpha-1729 <10110011100011110000@tutanota.com>
2023-08-14 00:45:55 +0530

    Half way  Section 11: Github Basics

---
+++ 062-GITHUB-Cloning_Github_Repos/Program.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Cloning Github Repos
+
+:"
+>>>> Hiring Without Whiteboards
+        A github repo that contains list of all companies that doesn't have general hiring process.
+        Reference Link: https://github.com/poteto/hiring-without-whiteboards
+>>>> Git clone Docs:
+        Reference Link: https://git-scm.com/docs/git-clone
+>>>>
+>>>>
+"
---
+++ 066-GITHUB-Crash_Course_On_Git_Remotes/Program.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Crash Course On Git Remotes
+
+:"
+>>>> By adding multiple remotes in the git repo.
+        We can actually upload same branch to multiple github repo.
+>>>>
+>>>>
+>>>>
+"
