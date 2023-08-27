f118c8e7b484a3af32f66c234a2dfbef82c74ca3
Alpha-1729 <10110011100011110000@tutanota.com>
2023-08-28 02:00:44 +0530

    Completed Section 14: Collaboration workflow

---
+++ 088-GITHUB-Resolving_Mr_With_Conflicts/Program.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Resolving Mr With Conflicts
+
+:"
+>>>> Your can directly resolve conflicts from the github UI.
+>>>> Or you can resolve conflicts from the command line and push the master branch to origin.
+        Please check the screenshot attached.
+        --no-ff  -> means to avoid the fast-forwarded merge to create a commit message while merging feature-branch into main.
+>>>>
+>>>>
+"
---
+++ 089-GITHUB-Configuring_Branch_Protection_Rules/Program.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Configuring Branch Protection Rules
+
+:"
+>>>>
+>>>>
+>>>>
+>>>>
+"
