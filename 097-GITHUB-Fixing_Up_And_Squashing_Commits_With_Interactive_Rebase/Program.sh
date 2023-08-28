022e5592d2f1cb1fc6148591a0d5c9da6ddb3596
Alpha-1729 <10110011100011110000@tutanota.com>
2023-08-28 17:54:14 +0530

    Completed Section 16: Interactive rebase

---
+++ 097-GITHUB-Fixing_Up_And_Squashing_Commits_With_Interactive_Rebase/Program.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Fixing Up And Squashing Commits With Interactive Rebase
+
+:"
+>>>> For sqashing commits.
+        git rebase -i HEAD~9
+            Use the keyword "fixup" to use the previous commit message and discard the current commit message.
+            Please refer the attached screenshot.
+>>>>
+>>>>
+>>>>
+"
