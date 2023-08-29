92ff41b9934c453cd3bb35da48fd923bd7d57b23
Alpha-1729 <10110011100011110000@tutanota.com>
2023-08-29 14:32:16 +0530

    Completed Section 20: Git aliases

---
+++ 130-GIT-Exploring_Existing_Useful_Alias_Online/Program.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Exploring Existing Useful Alias Online
+
+:"
+>>>> Custom git alias reference.
+        https://github.com/GitAlias/gitalias
+        https://gist.github.com/mwhite/6887990
+        https://www.durdn.com/blog/2012/11/22/must-have-git-aliases-advanced-examples/
+
+>>>>
+>>>>
+>>>>
+"
